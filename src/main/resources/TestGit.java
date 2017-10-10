cad58cd2f70e7987cce8df85ba4da6e65055649c
nghoiban <nghoiban@outlook.com>
2017-10-10 11:50:18 +0800

    The first commit.

---
+++ src/main/resources/TestGit.java
@@ -0,0 +1,7 @@
+import javax.xml.bind.SchemaOutputResolver;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.print("Just a test about git push.");
+    }
+}
